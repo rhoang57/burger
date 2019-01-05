e05f9a503bd7eb3f4b322edeed038d49d2c69390
rhoang57 <rhoang57@gmail.com>
2019-01-05 09:33:45 -0800

    locally working app

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Quarter Pounder with Cheese", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Whopper with Cheese", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bacon Cheeseburger", false);
